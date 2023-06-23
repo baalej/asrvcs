93e023dd5a36416db6ba37e58ff497ec27c9d9cb
Alejandro <abample@gmail.com>
2023-06-23 15:51:52 +0200

    update assets

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,24 +1,10 @@
-// const caption = document.querySelector('.caption')
-// const main = document.querySelector('main')
-// const images = document.querySelectorAll('.gallery img')
-// const imagesLength = images.length;
-// let counter = 0
-
-// function handleClick() {
-//   images[counter].style.display = 'none'
-//   counter = (counter + 1 < imagesLength) ? counter + 1 : 0
-//   images[counter].style.display = 'inline'
-//   const text = images[counter].getAttribute('data-caption')
-//   caption.innerHTML = text
-// }
-
-// main.addEventListener('click', handleClick, false)
-
 const previousButton = document.querySelector('.previous')
 const nextButton = document.querySelector('.next')
+const gallery = document.querySelector('.gallery')
 
 function previous() {
   console.log('previous')
+
 }
 
 function next() {
